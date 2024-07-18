bba8850803286171471791d919ee95efbe791cee
japygo <hyunnjinn@gmail.com>
2024-07-18 17:50:28 +0900

    테스트와 DI (1)

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -2,31 +2,32 @@ package tobyspring.hellospring.payment;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import tobyspring.hellospring.exrate.WebApiExRateProvider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 class PaymentServiceTest {
 
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
-    void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+    void convertedAmount() throws IOException {
+        testAmount(BigDecimal.valueOf(500), BigDecimal.valueOf(5_000));
+        testAmount(BigDecimal.valueOf(1_000), BigDecimal.valueOf(10_000));
+        testAmount(BigDecimal.valueOf(3_000), BigDecimal.valueOf(30_000));
+
+        // 원화환산금액의 유효시간 계산
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+    }
+
+    private void testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
-        // 환율정보 가져온다
-        assertThat(payment.getExRate()).isNotNull();
-
-        // 원화환산금액 계산
-        assertThat(payment.getConvertedAmount())
-                .isEqualTo(payment.getExRate().multiply(payment.getForeignCurrencyAmount()));
-
-        // 원화환산금액의 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+        assertThat(payment.getExRate()).isEqualByComparingTo(exRate);
+        assertThat(payment.getConvertedAmount()).isEqualByComparingTo(convertedAmount);
     }
 }
